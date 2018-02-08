3576ad49da8516d9149aa3d7e833ab1c44601481
SHINING <jack7770126@gmail.com>
2018-02-08 13:30:52 +0800

    [page] finish the index page.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,1 @@
-"use strict";$(document).ready(function(){new Vue({el:"#navbar",data:{navActive:"index"},mounted:function(){$("#navbar").removeClass("invisible")},methods:{}})});
+"use strict";$(document).ready(function(){new Vue({el:"#navbar",data:{navActive:"index"},mounted:function(){$("#navbar").removeClass("invisible")},methods:{}}),new Vue({el:"#indexPending",data:{pendingList:[{id:1,name:"采购立项",number:"CG2131521323213",status:"未审核"},{id:2,name:"采购立项",number:"CG73452321523213",status:"未审批"}]},mounted:function(){$("#indexPending").removeClass("invisible")}})});
