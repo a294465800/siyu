1554c875026ce8fa6d869844c95a70164563a0da
SHINING <jack7770126@gmail.com>
2018-02-09 14:36:40 +0800

    [page] add the select option in buy charge list page.

--- src/js/buy_charge_list.js
+++ src/js/buy_charge_list.js
@@ -1,1 +1,1 @@
-"use strict";$(document).ready(function(){new Vue({el:"#navbar",data:{navActive:"buyChargeList"},mounted:function(){$("#navbar").removeClass("invisible"),$(".ui.checkbox").checkbox()},methods:{}})});
+"use strict";$(document).ready(function(){new Vue({el:"#navbar",data:{navActive:"buyChargeList"},mounted:function(){$("#navbar").removeClass("invisible"),$(".ui.checkbox").checkbox(),$(".ui.dropdown").dropdown()},methods:{}})});
