013152c018ae55f237eb3403b0a401d60f56e993
SHINING <jack7770126@gmail.com>
2018-02-10 16:30:22 +0800

    [page] add the dialog in buy payment create page.

--- assets/js/buy_payment_create.js
+++ assets/js/buy_payment_create.js
@@ -4,11 +4,64 @@
       new Vue({
         el: '#paymentCreate',
         data: {
-          pay_date: ''
+          form: {
+            pay_date: '',
+            amount: ''
+          },
+          checkedMen: [],
+          menList: [{
+              id: 1,
+              name: '张先生'
+            },
+            {
+              id: 2,
+              name: '陈一发'
+            },
+            {
+              id: 3,
+              name: '刘芳芳'
+            },
+            {
+              id: 4,
+              name: '乌达奇'
+            },
+            {
+              id: 5,
+              name: '何求'
+            }
+          ],
         },
         mounted() {
-          this.pay_date = $('#hiddenDate').val() || ''
+          this.form.pay_date = $('#hiddenDate').val() || ''
+          this.form.amount = $('#hiddenAmount').val() || ''
         },
+        methods: {
+
+          //提交
+          submitForm() {
+            this.$notify({
+              title: '成功',
+              message: '提交成功！',
+              type: 'success'
+            })
+            $('.ui.dimmer').addClass('active')
+          },
+
+          //选择审核人
+          handleCheckManChange(value) {
+            console.log(this.checkedMen)
+          },
+
+          //提交审核人
+          confirmRecheck() {
+            this.$notify({
+              title: '成功',
+              message: '已选择了复核人',
+              type: 'success'
+            })
+            $('.ui.dimmer').removeClass('active')
+          }
+        }
       })
     })
 }()
