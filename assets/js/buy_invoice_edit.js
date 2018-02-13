b070a5fe842a7da27ecaf1f7d3b5b56bc3e03266
SHINING <jack7770126@gmail.com>
2018-02-13 11:38:33 +0800

    [page] finish the buy invoice edit page.

---
+++ assets/js/buy_invoice_edit.js
@@ -0,0 +1,23 @@
+! function () {
+  $(document)
+    .ready(() => {
+
+      new Vue({
+        el: '#buyInvoiceEdit',
+        data: {
+          form: {
+            get_date: '',
+            invoice_date: '',
+            type: ''
+          }
+        },
+        mounted() {
+          this.form.get_date = $('#getDate').val()
+          this.form.invoice_date = $('#invoiceDate').val()
+          this.form.type = $('#invoiceType').val()
+          $('#buyInvoiceEdit').removeClass('invisible')
+        },
+        methods: {}
+      })
+    })
+}()
