3d96b3ed0acf765feb6fab4cb4cd7a4745e17374
SHINING <jack7770126@gmail.com>
2018-02-27 22:07:45 +0800

    [page] finish the build pay apply page.

---
+++ assets/js/build_pay_apply.js
@@ -0,0 +1,67 @@
+! function () {
+  $(document)
+    .ready(() => {
+      new Vue({
+        el: '#buildPayApply',
+        data: {
+          applyForm: {
+            date: _helper.timeFormat(new Date(), 'YYY-MM-DD'),
+            amount: '',
+            payment: '',
+            bank: '',
+            account: ''
+          },
+
+          checkedMen: [],
+          menList: [{
+              id: 1,
+              name: '张先生'
+            },
+            {
+              id: 2,
+              name: '陈一发'
+            },
+            {
+              id: 3,
+              name: '刘芳芳'
+            },
+            {
+              id: 4,
+              name: '乌达奇'
+            },
+            {
+              id: 5,
+              name: '何求'
+            }
+          ],
+        },
+        mounted() {},
+        methods: {
+
+          submit() {
+            this.$notify({
+              title: '成功',
+              message: '提交成功',
+              type: 'success'
+            })
+            $('.ui.dimmer').addClass('active')
+          },
+
+          //选择审核人
+          handleCheckManChange(value) {
+            console.log(this.checkedMen)
+          },
+
+          //提交审核人
+          confirmRecheck() {
+            this.$notify({
+              title: '成功',
+              message: '已选择了复核人',
+              type: 'success'
+            })
+            $('.ui.dimmer').removeClass('active')
+          }
+        }
+      })
+    })
+}()
