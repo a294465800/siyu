90fbe25aa570f4efe82d944260a1c4d536c4e507
SHINING <jack7770126@gmail.com>
2018-02-28 14:35:03 +0800

    [page] finish the build get edit page.

---
+++ src/js/build_get_edit.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){new Vue({el:"#buildGetEdit",data:{form:{get_date:"",invoice_date:"",type:""}},mounted:function(){this.form.get_date=$("#getDate").val(),this.form.invoice_date=$("#invoiceDate").val(),this.form.type=$("#invoiceType").val(),$("#buildGetEdit").removeClass("invisible")},methods:{}})});
