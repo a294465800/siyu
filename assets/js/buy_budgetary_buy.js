a82119cfeb42a8927d2363f0f407e2eecc895955
SHINING <jack7770126@gmail.com>
2018-04-10 11:22:02 +0800

    [*] perfect buy_buygetary page.

--- assets/js/buy_budgetary_buy.js
+++ assets/js/buy_budgetary_buy.js
@@ -8,82 +8,8 @@
 
           invoiceType: [],
 
-          suppliers: [{
-              id: 1,
-              name: '供货商一',
-              bank: '中国银行',
-              account: 63432423423423234
-            },
-            {
-              id: 2,
-              name: '供货商二',
-              bank: '平安银行',
-              account: 63432523123423234
-            },
-            {
-              id: 3,
-              name: '供货商三',
-              bank: '广发银行',
-              account: 63432423213123234
-            },
-            {
-              id: 4,
-              name: '供货商四',
-              bank: '建设银行',
-              account: 63452131252133234
-            }
-          ],
-
           //物料
-          materials: [{
-              id: 1,
-              name: '物料一',
-              parameter: '参数一',
-              model: '型号一',
-              manufacturer: '厂家一',
-              unit: '个',
-              price: 253,
-              quantity: 2534,
-              buy_number: 1500,
-              left_number: 1034,
-            },
-            {
-              id: 2,
-              name: '物料二',
-              parameter: '参数二',
-              model: '型号二',
-              manufacturer: '厂家二',
-              unit: '件',
-              price: 2542,
-              quantity: 500,
-              buy_number: 300,
-              left_number: 200,
-            },
-            {
-              id: 4,
-              name: '物料三',
-              parameter: '参数三',
-              model: '型号三',
-              manufacturer: '厂家三',
-              unit: '间',
-              price: 123,
-              quantity: 5000,
-              buy_number: 2300,
-              left_number: 2700,
-            },
-            {
-              id: 3,
-              name: '物料四',
-              parameter: '参数四',
-              model: '型号四',
-              manufacturer: '厂家四',
-              unit: '间',
-              price: 542,
-              quantity: 5000,
-              buy_number: 5000,
-              left_number: 0,
-            }
-          ],
+          materials: [],
 
           //复核人dialog
           checkedMen: [],
@@ -108,11 +34,17 @@
               name: '何求'
             }
           ],
+
+          throttle: {
+            supplier_timer: null,
+          }
         },
         mounted() {
           this.budgetary_buy.info.date = _helper.timeFormat(new Date(), 'YYYY-MM-DD')
           const invoiceType = $('#invoiceType').text().trim()
           this.invoiceType = invoiceType === '' ? [] : JSON.parse(invoiceType)
+          const materials = $('#materials').text().trim()
+          this.materials = materials === '' ? [] : JSON.parse(materials)
           this.budgetary_buy.project_id = $('#projectId').val()
           $('#budgetaryBuy').removeClass('invisible')
         },
@@ -135,45 +67,62 @@
             for (let i in list) {
               let item = list[i]
               let material = item.material
-              if (typeof item.real_quantity === 'undefined') break
-              sum += parseFloat(item.real_amount || 0)
+              if (typeof item.number === 'undefined') break
+              sum += parseFloat(item.cost || 0)
               let materialIndex = material.index
-              result[materialIndex].left_number -= parseInt(item.real_quantity || 0)
+              result[materialIndex].need_number -= parseInt(item.number || 0)
             }
             vm.budgetary_buy.amount = sum || 0
             return result
           }
         },
         methods: {
-
-          //供货商搜索
-          querySearchSupplier(queryString, cb) {
-            var suppliers = this.suppliers
-            var results = queryString ? suppliers.filter(this.createFilterSupplier(queryString)) : suppliers;
-            // 调用 callback 返回建议列表的数据
-            clearTimeout(this.timeout);
-            this.timeout = setTimeout(() => {
-              cb(results);
-            }, 1000 * Math.random());
-          },
-          createFilterSupplier(queryString) {
-            return (item) => {
-              return (item.id.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-            };
-          },
+          
+          //供应商输入提示
           handleSelectSupplier(item) {
-            this.budgetary_buy.supplier.id = item.id
-            this.budgetary_buy.supplier.name = item.name
-            this.budgetary_buy.supplier.bank = item.bank
-            this.budgetary_buy.supplier.account = item.account
+            this.extrabudgetary.info.supplier_id = item.id
+            this.extrabudgetary.info.supplier_name = item.name
+            this.extrabudgetary.info.bank = item.bank
+            this.extrabudgetary.info.account = item.account
+          },
+          querySearchSupplier(queryString, cb) {
+
+            if (this.throttle.supplier_timer) {
+              clearTimeout(this.throttle.supplier_timer)
+            }
+            this.throttle.supplier_timer = setTimeout(() => {
+              const searchKey = {
+                name: queryString
+              }
+              _http.SupplierManager.searchSuppliers(searchKey)
+                .then(res => {
+                  if (res.data.code === '200') {
+                    cb(res.data.data)
+                  } else {
+                    this.$notify({
+                      title: '错误',
+                      message: res.data.msg || '未知错误',
+                      type: 'error'
+                    })
+                  }
+                })
+                .catch(err => {
+                  this.$notify({
+                    title: '错误',
+                    message: '服务器出错',
+                    type: 'error'
+                  })
+                })
+            }, 500)
           },
 
           //物料选择
           addMaterial(item, index) {
             const list = this.budgetary_buy.lists
             let data = {
-              id: list.length > 0 ? list[list.length - 1].id ? list[list.length - 1].id + 1 : 1 : 1,
-              material: item
+              own_id: list.length > 0 ? list[list.length - 1].own_id ? list[list.length - 1].own_id + 1 : 1 : 1,
+              material: item,
+              material_id: item.id
             }
             data.material.index = index
             this.budgetary_buy.lists.push(data)
