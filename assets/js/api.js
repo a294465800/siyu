a35e3d8810a95db8c2cfc5038c623e61c6da8491
SHINING <jack7770126@gmail.com>
2018-04-17 14:22:38 +0800

    [+] 预算添加检索功能。

--- assets/js/api.js
+++ assets/js/api.js
@@ -113,14 +113,20 @@
     }
 
     //预算内采购项目材料
-    searchBudgetMaterial(id, search = {}){
+    searchBudgetMaterial(id, search = {}) {
       return this._http.get(`/search/budget?project=${id}`, {
         params: search
       })
     }
 
-    searchPurchase(id){
+    searchPurchase(id) {
       return this._http.get(`/search/purchase?budget=${id}`)
+    }
+
+    searchMaterial(search = {}) {
+      return this._http.get(`/search/material`, {
+        params: search
+      })
     }
   }
 
