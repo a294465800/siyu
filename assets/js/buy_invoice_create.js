9a695012744d810ad26e2266491d48782104c786
SHINING <jack7770126@gmail.com>
2018-05-30 12:42:26 +0800

    fix

--- assets/js/buy_invoice_create.js
+++ assets/js/buy_invoice_create.js
@@ -42,6 +42,9 @@
                     message: `提交成功`,
                     type: 'success'
                   })
+                  setTimeout(() => {
+                    window.close();
+                  }, 2000)
                 } else {
                   this.$notify({
                     title: '错误',
