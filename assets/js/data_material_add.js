16bdc00cae7f4b9f8cd327ce1f04228b3eeff4fe
SHINING <jack7770126@gmail.com>
2018-07-18 23:47:25 +0800

    [+] 新增物料搜索

--- assets/js/data_material_add.js
+++ assets/js/data_material_add.js
@@ -22,6 +22,43 @@
           this.materialForm.unit = $('#materialUnit').val() || ''
         },
         methods: {
+
+
+          //物料
+          querySearchMaterial(queryString, cb) {
+            if (this.throttle.material_timer) {
+              clearTimeout(this.throttle.material_timer)
+            }
+            this.throttle.material_timer = setTimeout(() => {
+              const searchKey = {
+                name: queryString
+              }
+              _http.MaterialManager.searchMaterial(searchKey)
+                .then(res => {
+                  if (res.data.code === '200') {
+                    cb(res.data.data)
+                  } else {
+                    this.$notify({
+                      title: '错误',
+                      message: res.data.msg || '未知错误',
+                      type: 'error'
+                    })
+                  }
+                })
+                .catch(err => {
+                  this.$notify({
+                    title: '错误',
+                    message: '服务器出错',
+                    type: 'error'
+                  })
+                })
+            }, 500)
+          },
+          handleSelectMaterial(item) {
+            return false
+          },
+
+
           //提交
           submit() {
             _http.MaterialManager.createMaterial(this.materialForm)
